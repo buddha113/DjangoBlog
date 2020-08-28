275d7520f18fe86d16a902ad9dbcc136bdd0274d
Srijan Pokhrel <srijan.pokhrel113@gmail.com>
2020-08-28 16:03:52 +0545

    final Commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -137,8 +137,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="doctorloki75@gmail.com"
-EMAIL_HOST_PASSWORD="ecbjkdwwcrujthnx"
+EMAIL_HOST_USER="MYEMAIL"
+EMAIL_HOST_PASSWORD="MYPASSWORD"
 # DEFAUTL_FROM_EMAIL="Srijan Pokhrel Team <srijan.pokhrel113@gmail.com>"
 
 
